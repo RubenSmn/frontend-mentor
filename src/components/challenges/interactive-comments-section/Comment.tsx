7193b9737e6ba9738437de935b8ccff192309dbf
RubenSmn <saaman.ruben@gmail.com>
2023-02-19 18:02:55 +0100

    feat(interactive-comments-section): add update functionality

--- src/components/challenges/interactive-comments-section/Comment.tsx
+++ src/components/challenges/interactive-comments-section/Comment.tsx
@@ -2,6 +2,7 @@ import { useStore } from "@nanostores/react";
 import { useState } from "react";
 import { humaninzedTimeAgo } from "../../../helpers/time";
 import { useModal } from "../../../hooks/challenges/interactive-comments-section/useModal";
+import EditArea from "./EditArea";
 import ReplySection from "./ReplySection";
 import { CommentType, currentUser, normalizedComments } from "./Store";
 
@@ -27,6 +28,7 @@ function createComment(
 
 function Comment({ id, src }: Props) {
   const [showReply, setShowReply] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
   const [voteStatus, setVoteStatus] = useState(0);
   const { open } = useModal(onDelete);
   const $comments = useStore(normalizedComments);
@@ -49,8 +51,10 @@ function Comment({ id, src }: Props) {
     normalizedComments.set(updatedComments);
   }
 
-  function handleEdit() {
-    console.log("editing");
+  function handleEdit(newContent: string) {
+    const updatedComment = { ...comment, content: newContent };
+    normalizedComments.setKey(id, updatedComment);
+    setShowEdit(false);
   }
 
   function handleUpVote() {
@@ -88,67 +92,78 @@ function Comment({ id, src }: Props) {
             {comment.createdAt}
           </span>
         </header>
-        <p className="text-[var(--grayish-blue)] mb-4">
-          {comment.replyingTo ? (
-            <span className="text-[var(--moderate-blue)] font-[500]">
-              @{comment.replyingTo}{" "}
-            </span>
-          ) : null}
-          {comment.content}
-        </p>
-        <footer className="flex justify-between items-center">
-          <div className="flex justify-evenly items-center bg-[var(--very-light-gray)] rounded-lg">
-            <button
-              className={`px-4 py-3 fill-[#C5C6EF] hover:fill-[var(--moderate-blue)] ease-linear duration-150${
-                voteStatus === 1 ? " fill-[var(--moderate-blue)]" : ""
-              }`}
-              onClick={handleUpVote}
-            >
-              <svg viewBox="0 0 11 11" height="11" width="11">
-                <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"></path>
-              </svg>
-            </button>
-            <span className="text-[var(--moderate-blue)] font-bold">
-              {comment.score + voteStatus}
-            </span>
-            <button
-              className={`px-4 py-3 fill-[#C5C6EF] hover:fill-[var(--moderate-blue)] ease-linear duration-150${
-                voteStatus === -1 ? " fill-[var(--moderate-blue)]" : ""
-              }`}
-              onClick={handleDownVote}
-            >
-              <svg viewBox="0 0 11 3" height="3" width="11">
-                <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"></path>
-              </svg>
-            </button>
-          </div>
-          {isCurrentUser ? (
-            <div className="flex gap-4">
-              <button
-                className="flex justify-between items-center gap-2 text-[var(--soft-red)] font-bold hover:opacity-70 ease-in duration-150"
-                onClick={open}
-              >
-                <img src={`${src}/icon-delete.svg`} alt="Reply icon" />
-                Delete
-              </button>
-              <button
-                className="flex justify-between items-center gap-2 text-[var(--moderate-blue)] font-bold hover:opacity-70 ease-in duration-150"
-                onClick={handleEdit}
-              >
-                <img src={`${src}/icon-edit.svg`} alt="Reply icon" />
-                Edit
-              </button>
-            </div>
-          ) : (
-            <button
-              className="flex justify-between items-center gap-2 text-[var(--moderate-blue)] font-bold hover:opacity-70 ease-in duration-150"
-              onClick={() => setShowReply((prev) => !prev)}
-            >
-              <img src={`${src}/icon-reply.svg`} alt="Reply icon" />
-              Reply
-            </button>
-          )}
-        </footer>
+        {showEdit ? (
+          <EditArea
+            comment={comment.content}
+            onSubmit={handleEdit}
+            src={src}
+            onCancel={() => setShowEdit(false)}
+          />
+        ) : (
+          <>
+            <p className="text-[var(--grayish-blue)] mb-4">
+              {comment.replyingTo ? (
+                <span className="text-[var(--moderate-blue)] font-[500]">
+                  @{comment.replyingTo}{" "}
+                </span>
+              ) : null}
+              {comment.content}
+            </p>
+            <footer className="flex justify-between items-center">
+              <div className="flex justify-evenly items-center bg-[var(--very-light-gray)] rounded-lg">
+                <button
+                  className={`px-4 py-3 fill-[#C5C6EF] hover:fill-[var(--moderate-blue)] ease-linear duration-150${
+                    voteStatus === 1 ? " fill-[var(--moderate-blue)]" : ""
+                  }`}
+                  onClick={handleUpVote}
+                >
+                  <svg viewBox="0 0 11 11" height="11" width="11">
+                    <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"></path>
+                  </svg>
+                </button>
+                <span className="text-[var(--moderate-blue)] font-bold">
+                  {comment.score + voteStatus}
+                </span>
+                <button
+                  className={`px-4 py-3 fill-[#C5C6EF] hover:fill-[var(--moderate-blue)] ease-linear duration-150${
+                    voteStatus === -1 ? " fill-[var(--moderate-blue)]" : ""
+                  }`}
+                  onClick={handleDownVote}
+                >
+                  <svg viewBox="0 0 11 3" height="3" width="11">
+                    <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"></path>
+                  </svg>
+                </button>
+              </div>
+              {isCurrentUser ? (
+                <div className="flex gap-4">
+                  <button
+                    className="flex justify-between items-center gap-2 text-[var(--soft-red)] font-bold hover:opacity-70 ease-in duration-150"
+                    onClick={open}
+                  >
+                    <img src={`${src}/icon-delete.svg`} alt="Reply icon" />
+                    Delete
+                  </button>
+                  <button
+                    className="flex justify-between items-center gap-2 text-[var(--moderate-blue)] font-bold hover:opacity-70 ease-in duration-150"
+                    onClick={() => setShowEdit(true)}
+                  >
+                    <img src={`${src}/icon-edit.svg`} alt="Reply icon" />
+                    Edit
+                  </button>
+                </div>
+              ) : (
+                <button
+                  className="flex justify-between items-center gap-2 text-[var(--moderate-blue)] font-bold hover:opacity-70 ease-in duration-150"
+                  onClick={() => setShowReply((prev) => !prev)}
+                >
+                  <img src={`${src}/icon-reply.svg`} alt="Reply icon" />
+                  Reply
+                </button>
+              )}
+            </footer>
+          </>
+        )}
       </section>
       {showReply ? (
         <ReplySection src={src} type={"Reply"} onReply={onReply} />
---
+++ src/components/challenges/interactive-comments-section/EditArea.tsx
@@ -0,0 +1,41 @@
+import type React from "react";
+
+type Props = {
+  comment: string;
+  onSubmit: (updatedComment: string) => void;
+  onCancel: () => void;
+  src: string;
+};
+
+function EditArea({ comment, onSubmit, onCancel }: Props) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    onSubmit(formData.get("comment") as string);
+  }
+
+  return (
+    <form className="grid grid-cols-2 gap-4" onSubmit={handleSubmit}>
+      <fieldset className="col-span-2">
+        <textarea
+          name="comment"
+          rows={3}
+          placeholder="Add a comment..."
+          defaultValue={comment}
+          className="border-2 border-[var(--light-gray)] text-[var(--grayish-blue)] rounded-lg py-2 px-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-[var(--moderate-blue)] placeholder:font-[500] w-full"
+        ></textarea>
+      </fieldset>
+      <button
+        className="px-4 py-3 rounded-md uppercase font-[500] text-white bg-[var(--grayish-blue)] self-center justify-self-start hover:opacity-70 ease-in duration-150"
+        onClick={onCancel}
+      >
+        Cancel
+      </button>
+      <button className="px-4 py-3 rounded-md uppercase font-[500] text-white bg-[var(--moderate-blue)] self-center justify-self-end hover:opacity-70 ease-in duration-150">
+        Update
+      </button>
+    </form>
+  );
+}
+
+export default EditArea;
